23df11f6fc4871c3e446b600acaf097989ba14e7
Reet Mitra <63037606+reetmitra@users.noreply.github.com>
2026-06-10 13:32:08 +0800

    test: add IS_REACT_ACT_ENVIRONMENT to jest.setup to suppress act warnings

--- web/jest.setup.ts
+++ web/jest.setup.ts
@@ -1,2 +1,4 @@
 import '@testing-library/jest-dom'
 jest.mock('next/navigation', () => ({ useRouter: () => ({}) }))
+// Suppress React 18 act() warnings in jsdom test environment
+global.IS_REACT_ACT_ENVIRONMENT = true
